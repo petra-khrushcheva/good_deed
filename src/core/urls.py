8881339c7e80c0726d53183f117d3d7c4ecba9dc
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-05-18 19:24:38 +0500

    add urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("collects/", include("collects.urls")),
+    path("funds/", include("funds.urls")),
+    path("payments/", include("payments.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
