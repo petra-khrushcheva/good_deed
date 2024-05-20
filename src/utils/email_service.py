08adc9d1646118ad971532508fba966d25731be9
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-05-20 21:41:17 +0500

    change email functions to celery tasks

--- src/utils/email_service.py
+++ src/utils/email_service.py
@@ -1,25 +1,51 @@
-import functools
+from functools import wraps
+
+from celery import shared_task
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+
+CREATE_COLLECT_SUBJECT = "Вы создали сбор!"
+CREATE_COLLECT_BODY_TEMPLATE = (
+    "{}! Вы создали сбор {} в пользу фонда {}. Спасибо вам за вашу помощь!"
+)
+MAKE_PAYMENT_SUBJECT = "Спасибо за вашу помощь!"
+MAKE_PAYMENT_BODY_TEMPLATE = (
+    "{} {}! Вы совершили платеж на сумму {} в пользу фонда {}. "
+    "Фонд и {} благодарны вам за вашу помощь."
+)
 
 
-# написать декоратор
-
-
-def mail_template(subject: str, message: str):
+def create_mail(subject, body_template):
     def decorator(func):
-        @functools.wraps(func)
-        def wrapper(recipient_list):
-            return func(
+        @wraps(func)
+        def wrapper(email_to, *args, **kwargs):
+            email = EmailMessage(
                 subject=subject,
-                message=message,
+                body=body_template.format(*args, **kwargs),
                 from_email=settings.EMAIL_HOST_USER,
-                recipient_list=recipient_list,
+                to=[email_to],
             )
+            result = email.send()
+            return result
+
         return wrapper
+
     return decorator
 
 
-@mail_template(subject="Создание сбора", message="Вы создали сбор")
-def collect_create_mail(recipient_list):
-    send_mail(recipient_list=recipient_list)
+@shared_task
+@create_mail(
+    subject=CREATE_COLLECT_SUBJECT, body_template=CREATE_COLLECT_BODY_TEMPLATE
+)
+def send_create_collect_mail(email_to, full_name, collect_name, fund_name):
+    return
+
+
+@shared_task
+@create_mail(
+    subject=MAKE_PAYMENT_SUBJECT, body_template=MAKE_PAYMENT_BODY_TEMPLATE
+)
+def send_make_payment_mail(
+    email_to, first_name, last_name, amount, fund_name, organizer_name
+):
+    return
