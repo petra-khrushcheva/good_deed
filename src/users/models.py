c4d9843f275c2f5dc788b297a6585333b96c8686
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-05-20 21:45:28 +0500

    add class meta to user model

--- src/users/models.py
+++ src/users/models.py
@@ -4,5 +4,10 @@ from core.basemodels import BaseModel
 
 
 class User(BaseModel, AbstractUser):
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
     def __str__(self) -> str:
         return self.get_full_name()
